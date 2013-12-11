777762cb7279f63410b245738d62165dca748cf3
Ilya Biin <i.biin@ostrovok.ru>
2013-12-11 21:24:25 +0400

    builf fixes

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 GF=$(mktemp -t goenvXXX); curl -s https://raw.github.com/biinilya/goenv/master/activate.sh > $GF; source $GF
 goon otacmgr
+goversion 1.2
 set -x
 
-$GOBIN/go get -fix "github.com/coreos/etcd"
-$GOBIN/go get -fix "github.com/coreos/etcdctl"
+rm -rf ~/.goenv/otacmgr
+go get "github.com/coreos/etcd"
+go get "github.com/coreos/etcdctl"
 
 CDIR=$(pwd)
 cd $GOENV_PATH/src/github.com/coreos/etcd
